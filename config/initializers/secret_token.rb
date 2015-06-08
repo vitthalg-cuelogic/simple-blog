62235ee5191c154b4854256916eb23c6c16f01d8
Vitthal Gaikwad <vitthal.gaikwad@cuelogic.co.in>
2015-06-08 11:47:08 +0530

    Add rails directory structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogExercise::Application.config.secret_key_base = 'e289c3ade18b855314675853bf8a9390c242250bf84b36df4c54114fa5215a0bffe197405f82c5761027a176513c6f7f3294912fe47dc1ae0d9637e4f1f5adf2'
