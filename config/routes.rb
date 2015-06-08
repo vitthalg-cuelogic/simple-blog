0cc2e40e437ff3164a6999fc13015633a924c48b
Vitthal Gaikwad <vitthal.gaikwad@cuelogic.co.in>
2015-06-08 13:04:29 +0530

    Generate home controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BlogExercise::Application.routes.draw do
+  get "home/index"
   namespace :admin do
     resources :blogs
   end
@@ -7,7 +8,7 @@ BlogExercise::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
